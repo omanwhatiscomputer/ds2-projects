645e6f7ca61da67866c037fb20f4f4218961efbf
omanwhatiscomputer <khubayeebkabeer@gmail.com>
2025-08-28 11:48:43 -0400

    SimpleLinearRegression

--- src/main/scala/project1/HelloWorld.scala
+++ src/main/scala/project1/HelloWorld.scala
@@ -24,13 +24,35 @@ def maxCol(m: MatrixD): VectorD = VectorD((0 until m.dim2).map(j => m.col(j).max
         .filter(row => row.forall(_.nonEmpty))
         .toArray
 
+    // // Extract y (first column) and convert to VectorD
+    // val y = VectorD(data.map(_(0).toDouble))
+
+    // // Extract x (remaining columns) and convert to MatrixD
+    // val xRows = data.map(row => row.drop(1).map(_.toDouble))
+    // val x = MatrixD(xRows.map(row => VectorD(row)).toIndexedSeq)
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
     // Extract y (first column) and convert to VectorD
     val y = VectorD(data.map(_(0).toDouble))
 
     // Extract x (remaining columns) and convert to MatrixD
     val xRows = data.map(row => row.drop(1).map(_.toDouble))
-    val x = MatrixD(xRows.map(row => VectorD(row)).toIndexedSeq)
-    
+    val xRaw = MatrixD(xRows.map(row => VectorD(row)).toIndexedSeq)
+
+    // Normalize x (min-max scaling for each column)
+    val xMin = minCol(xRaw)
+    val xMax = maxCol(xRaw)
+
+    val x = MatrixD((0 until xRaw.dim).map(i => {
+        val row = xRaw(i)
+        VectorD(row.zipWithIndex.map { case (v, j) =>
+            if (xMax(j) != xMin(j)) then (v - xMin(j)) / (xMax(j) - xMin(j)) else 0.0
+        })
+    }).toIndexedSeq)
+
+    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     // Print first 5 y values and x rows for verification
     println("y (displacement): " + y(0 until 5))
@@ -41,5 +63,5 @@ def maxCol(m: MatrixD): VectorD = VectorD((0 until m.dim2).map(j => m.col(j).max
     mod.trainNtest ()()                                            // train and test the model
 
     val yp = mod.predict (x)
-    println(yp)
+    println(yp(0))
     // new Plot (x(?, 1), y, yp, "plot y and yp vs. x", lines = true)
